feb10771c717459ddbabb4457ca42e125ed976e4
chavin07 <chavincc@hotmail.com>
2018-06-11 16:47:15 -0500

    segunda version de Hola mundito

--- HolaMundito/src/com/holamundito/main/HolaMundito.java
+++ HolaMundito/src/com/holamundito/main/HolaMundito.java
@@ -4,6 +4,8 @@ public class HolaMundito {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		System.out.println("Hola mundo2");
+		System.out.println("Hola mundo3");
 
 	}
 
